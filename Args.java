b6e5ceb4016336d3ab7ce2a548343caf8dc27c3f
BuiltWithRobK <rkelly3@citadel.edu>
2025-03-19 15:16:39 -0400

    java practice problems

---
+++ Args.java
@@ -0,0 +1,38 @@
+class Args
+{
+    public static void main(String[]args)
+    {
+        if (args.length != 3)
+        {
+            System.out.println("Wrong number of arguments");
+        }
+        
+        int num1 = Integer.parseInt(args[0]) ;
+        int num2 = Integer.parseInt(args[2]) ;
+
+        String msg = args[0] + args[1] + args[2] + "=" ;
+
+        if (args[1].equals("+")) 
+        {
+            msg += (num1 + num2);
+        } 
+        else if (args[1].equals("-")) 
+        {
+            msg += (num1 - num2);
+        } 
+        else if (args[1].equals("x")) 
+        {  
+            msg += (num1 * num2);
+        } 
+        else if (args[1].equals("/")) 
+        {  
+            msg += (num1 / num2);
+        } 
+        else 
+        {
+            msg = "Incorrect Operator"; 
+        }
+        
+        System.out.println(msg);
+    }
+}
