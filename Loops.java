be9775a1e6a7b0c3991bde403c2d4231069e85f1
BuiltWithRobK <robert.h.kelly3@gmail.com>
2025-03-26 14:28:08 -0400

    Java Practice Probs

---
+++ Loops.java
@@ -0,0 +1,25 @@
+class Loops {
+    public static void main (String[] args){
+        //if (args.length > 0){
+            //for (int i = 0; i < args.length; i++){
+                //System.out.println( "args[" +i+"] is | "+ args[i]);
+            //}
+        //}
+        String[] htm = {"HTML", "in", "easy", "steps"} ;
+        String[] xml = {"XML", "in", "easy", "steps"} ;
+
+        int j = 0 ;
+        int k = 0 ;
+
+        while (j < htm.length){
+            System.out.println("htm[" +j+"] is | " + htm[j]);
+            j++ ;
+        }
+
+        if (xml.length > 0) do {
+            System.out.println( "\t\txml[" +k+ "] is | " +xml[k] );
+            k++ ;
+        } while (k < xml.length);
+        
+    }
+}
