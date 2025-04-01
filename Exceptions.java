a226a79584b8c1c76bfc0f736e940cfdd82b124c
BuiltWithRobK <robert.h.kelly3@gmail.com>
2025-04-01 15:30:49 -0400

    practice workbook

---
+++ Exceptions.java
@@ -0,0 +1,13 @@
+
+class Exceptions {
+   public static void main(String[] args) {
+       try {
+        int num = Interger.parseint(args[0]);
+        System.out.println("You entered: " + num);
+        
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Interger")
+        }
+       }
+   } 
+}
